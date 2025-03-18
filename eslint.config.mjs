1cfcd7a80e59a1d050ecd6feb207e6cb58241599
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-18 12:38:09 +0100

    chore: eslint format

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {ignores: ['package-localStorage.json', 'playwright-report', 'test-results/**']},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+    }
+  },
+
+];
--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 
+console.log('Hello World');
+
 test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
 });
@@ -426,12 +428,14 @@ async function checkNumberOfTodosInLocalStorage(page: Page, expected: number) {
 
 async function checkNumberOfCompletedTodosInLocalStorage(page: Page, expected: number) {
   return await page.waitForFunction(e => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return JSON.parse(localStorage['react-todos']).filter((todo: any) => todo.completed).length === e;
   }, expected);
 }
 
 async function checkTodosInLocalStorage(page: Page, title: string) {
   return await page.waitForFunction(t => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return JSON.parse(localStorage['react-todos']).map((todo: any) => todo.title).includes(t);
   }, title);
 }
