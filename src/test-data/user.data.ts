e25ca50c688a48c984a6aff3e486c014b7833d08
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-20 15:15:45 +0100

    chore: test data for password user

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,4 +1,6 @@
 export const testUser1 = {
-  userEmail: 'Moses.Armstrong@Feest.ca',
-  //userEmail: process.env.USER_EMAIL,
+  //userEmail: 'Moses.Armstrong@Feest.ca',
+  userEmail: process.env.USER_EMAIL,
+  //   userPassword: 'test1',
+  userPassword: process.env.USER_PASSWORD,
 };
