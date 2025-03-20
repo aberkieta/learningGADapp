9e120fc2f20f8231e5ddd0c684a871b9be909684
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-20 11:42:22 +0100

    test: login scenario

---
+++ src/pages/login.page.ts
@@ -0,0 +1,20 @@
+import { BasePage } from './base.page';
+import { Page } from '@playwright/test';
+
+export class LoginPage extends BasePage {
+  url = 'login/';
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async login(email: string, password: string): Promise<void> {
+    await this.page
+      .getByRole('textbox', { name: 'Enter User Email' })
+      .fill(email);
+    await this.page
+      .getByRole('textbox', { name: 'Enter Password' })
+      .fill(password);
+    await this.page.getByRole('button', { name: 'LogIn' }).click();
+  }
+}
