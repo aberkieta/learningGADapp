dd612d5fd957f9b6e03d10f963176d8c5d4fbfb9
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-20 15:39:41 +0100

    test: incorrect login scenario

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -3,6 +3,7 @@ import { Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   url = 'login/';
+  loginError = this.page.getByTestId('login-error');
 
   constructor(page: Page) {
     super(page);
--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -19,4 +19,22 @@ test.describe('Verify login', () => {
     //Assert
     expect(title).toContain('Welcome');
   });
+
+  test('login with incorrect password @GAD_R02_01', async ({ page }) => {
+    //Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = 'incorrect Password';
+    const loginPage = new LoginPage(page);
+
+    //Act
+    await loginPage.goto();
+    await loginPage.login(userEmail, userPassword);
+
+    //Assert
+    await expect
+      .soft(loginPage.loginError)
+      .toHaveText('Invalid username or password');
+    const title = await loginPage.title();
+    expect.soft(title).toContain('Login');
+  });
 });
