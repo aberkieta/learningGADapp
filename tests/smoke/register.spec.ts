58b06b4700af8c11b6c0b2986746e66f357911bf
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-24 13:05:09 +0100

    test: register user

---
+++ tests/smoke/register.spec.ts
@@ -0,0 +1,39 @@
+import { LoginPage } from '../../src/pages/login.page';
+import { RegisterPage } from '../../src/pages/register.page';
+import { WelcomePage } from '../../src/pages/welcome.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify register', () => {
+  test('register with correct data and login @GAD_R32_01', async ({ page }) => {
+    //Arrange
+    const userFirstName = 'Agata';
+    const userLastName = 'Nowak';
+    const userEmail = `nowaczek1${new Date().getTime()}@nowa.pl`;
+    const userPassword = 'test123';
+    const registerPage = new RegisterPage(page);
+    //Act
+    await registerPage.goto();
+    await registerPage.register(
+      userFirstName,
+      userLastName,
+      userEmail,
+      userPassword,
+    );
+    const expectedAlertPopupText = 'User created';
+    //Assert
+    await expect(registerPage.alertPopup).toHaveText(expectedAlertPopupText);
+
+    const loginPage = new LoginPage(page);
+    await loginPage.waitForPageToLoadUrl();
+    const titleLogin = await loginPage.title();
+    expect.soft(titleLogin).toContain('Login');
+
+    //Assert
+
+    await loginPage.login(userEmail, userPassword);
+    const welcomePage = new WelcomePage(page);
+    const titleWelcome = await welcomePage.title();
+    //Assert
+    expect(titleWelcome).toContain('Welcome');
+  });
+});
