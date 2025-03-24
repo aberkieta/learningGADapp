31642ac205bea035a5905aa37c653eba8bfa51ca
Agata Berkieta <agata.berkieta@gmail.com>
2025-03-24 15:29:20 +0100

    chore: register uder model

--- src/pages/register.page.ts
+++ src/pages/register.page.ts
@@ -1,3 +1,4 @@
+import { RegisterUser } from '../models/user.model';
 import { BasePage } from './base.page';
 import { Locator, Page } from '@playwright/test';
 
@@ -53,16 +54,11 @@ export class RegisterPage extends BasePage {
     this.alertPopup = this.page.getByTestId('alert-popup');
   }
 
-  async register(
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-  ): Promise<void> {
-    await this.userFirstNameInput.fill(firstName);
-    await this.userLastNameInput.fill(lastName);
-    await this.userEmailInput.fill(email);
-    await this.userPasswordInput.fill(password);
+  async register(registerUserData: RegisterUser): Promise<void> {
+    await this.userFirstNameInput.fill(registerUserData.userFirstName);
+    await this.userLastNameInput.fill(registerUserData.userLastName);
+    await this.userEmailInput.fill(registerUserData.userEmail);
+    await this.userPasswordInput.fill(registerUserData.userPassword);
     await this.registerButton.click();
   }
 }
--- tests/smoke/register.spec.ts
+++ tests/smoke/register.spec.ts
@@ -1,3 +1,4 @@
+import { RegisterUser } from '../../src/models/user.model';
 import { LoginPage } from '../../src/pages/login.page';
 import { RegisterPage } from '../../src/pages/register.page';
 import { WelcomePage } from '../../src/pages/welcome.page';
@@ -6,24 +7,39 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
   test('register with correct data and login @GAD_R32_01', async ({ page }) => {
-    //Arrange
-    const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
-    const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
-    // const userEmail = `nowaczek1${new Date().getTime()}@nowa.pl`;
-    const userEmail = faker.internet.email({
-      firstName: userFirstName,
-      lastName: userLastName,
+    // //Arrange
+    // const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
+    // const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
+    // // const userEmail = `nowaczek1${new Date().getTime()}@nowa.pl`;
+    // const userEmail = faker.internet.email({
+    //   firstName: userFirstName,
+    //   lastName: userLastName,
+    // });
+    // const userPassword = faker.internet.password({ length: 5 });
+
+    const registerUserData: RegisterUser = {
+      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+      userEmail: '',
+      userPassword: faker.internet.password({ length: 5 }),
+    };
+
+    registerUserData.userEmail = faker.internet.email({
+      firstName: registerUserData.userFirstName,
+      lastName: registerUserData.userLastName,
     });
-    const userPassword = faker.internet.password({ length: 5 });
+
     const registerPage = new RegisterPage(page);
     //Act
     await registerPage.goto();
-    await registerPage.register(
-      userFirstName,
-      userLastName,
-      userEmail,
-      userPassword,
-    );
+    // await registerPage.register(
+    //   registerUserData.userFirstName,
+    //   registerUserData.userLastName,
+    //   registerUserData.userEmail,
+    //   registerUserData.userPassword,
+    // );
+
+    await registerPage.register(registerUserData);
     const expectedAlertPopupText = 'User created';
     //Assert
     await expect(registerPage.alertPopup).toHaveText(expectedAlertPopupText);
@@ -35,7 +51,10 @@ test.describe('Verify register', () => {
 
     //Assert
 
-    await loginPage.login(userEmail, userPassword);
+    await loginPage.login(
+      registerUserData.userEmail,
+      registerUserData.userPassword,
+    );
     const welcomePage = new WelcomePage(page);
     const titleWelcome = await welcomePage.title();
     //Assert
