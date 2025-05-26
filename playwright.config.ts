2d5a0e840e9968129d267f10df4bfcb00043842f
Agata Berkieta <agata.berkieta@gmail.com>
2025-05-26 14:58:44 +0200

    chore: adding new project

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,5 +29,13 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'iphone',
+      use: { ...devices['iPhone 12 Mini'] },
+    },
+    {
+      name: 'smoke',
+      testDir: './tests/smoke',
+    },
   ],
 });
